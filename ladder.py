bf7003a171ec585e358ba15c693200d449f02b69
damaoooo <45312642+damaoooo@users.noreply.github.com>
2024-05-04 05:06:08 +0300

    Update ladder.py

--- ladder.py
+++ ladder.py
@@ -31,7 +31,7 @@ def create_dns_file(dns_token: str, file_path: str = "./.dns_token"):
 def get_configs(password):
     r = requests.post("https://basic-bundle-curly-cherry-0f53.damaoooo.workers.dev/config_file", json={"password": password})
     if r.status_code == 200:
-        return r.json()['v2'], r.json()['trojan'], r.json()['nginx']
+        return r.json()['v2'], r.json()['user']
     else:
         print_red("Get configs failed!")
         return "", "", ""
@@ -46,78 +46,54 @@ def get_cert_abs_path(domain_name: str):
     return fullchain_path, key_path
 
 
-class TrojanConfig:
-    def __init__(self, trojan_config: dict):
-        self.trojan_config = trojan_config
-        
-    def update_trojan_config(self, domain_name: str):
-        
-        port = self.trojan_config["local_port"]
-        port = input("Input the trojan port(default: {}):".format(port)) or port
-        
-        link_path = f"/etc/letsencrypt/live/{domain_name}"
-        self.trojan_config["local_port"] = port
-        self.trojan_config["password"] = ["fuckyoub1tch"]
-        self.trojan_config["ssl"]["cert"] = os.path.join(link_path, "fullchain.pem")
-        self.trojan_config["ssl"]["key"] = os.path.join(link_path, "privkey.pem")
-    
-    def save_trojan_config(self, save_path: str):
-        with open(save_path, "w") as f:
-            content = json.dumps(self.trojan_config, indent=4)
-            f.write(content)
-
-
-class V2rayConfig:
-    def __init__(self, v2ray_config: dict):
-        self.v2ray_config = v2ray_config
+class XrayConfig:
+    def __init__(self, Xray_config: dict):
+        self.xray_config = Xray_config
         self.inner_port = 0
         self.ws_path = ""
         
-    def update_v2ray_config(self):
+    def update_xray_config(self, xray_name: str, cdn_name: str, user_dict: dict):
         
-        inner_port = self.v2ray_config["inbounds"][0]["port"]
-        ws_path = self.v2ray_config["inbounds"][0]["streamSettings"]["wsSettings"]["path"]
+        # update the certificate
+        xray_cert_file = f"/etc/letsencrypt/live/{xray_name}/fullchain.pem"
+        xray_cert_key = f"/etc/letsencrypt/live/{xray_name}/privkey.pem"
         
-        inner_port = input("Input the inner v2ray port(default: {}):".format(inner_port)) or inner_port
-        ws_path = input("Input the ws path(default: {}):".format(ws_path)) or ws_path
+        cdn_cert_file = f"/etc/letsencrypt/live/{cdn_name}/fullchain.pem"
+        cdn_cert_key = f"/etc/letsencrypt/live/{cdn_name}/privkey.pem"
         
-        self.v2ray_config["inbounds"][0]["port"] = inner_port
-        self.v2ray_config["inbounds"][0]["streamSettings"]["wsSettings"]["path"] = ws_path
+        xray_cert_setting = {
+                            "ocspStapling": 3600,
+                            "certificateFile": xray_cert_file,
+                            "keyFile": xray_cert_key
+                        }
+
+        cdn_cert_setting = {
+                            "ocspStapling": 3600,
+                            "certificateFile": cdn_cert_file,
+                            "keyFile": cdn_cert_key
+                        }
         
-        self.inner_port = inner_port
-        self.ws_path = ws_path
         
-    def save_v2ray_config(self, save_path: str):
+        self.xray_config['inbounds'][0]['streamSettings']['tlsSettings']['certificates'] = [xray_cert_setting, cdn_cert_setting]
+        
+        # set the user id
+        vision_reality_clients = []
+        ws_clients = []
+        
+        for username in user_dict.keys():
+            user_uuid = user_dict[username]
+            vision_reality_clients.append({"id": user_uuid, "flow": "xtls-rprx-vision", "email": f"{username}@qq.com"})
+            ws_clients.append({'id': user_uuid, "email": f"{username}@qq.com"})
+            
+        self.xray_config['inbounds'][0]['settings']['clients'] = vision_reality_clients
+        self.xray_config['inbounds'][1]['settings']['clients'] = vision_reality_clients
+        self.xray_config['inbounds'][2]['settings']['clients'] = ws_clients
+        
+        
+    def save_xray_config(self, save_path: str):
         with open(save_path, "w") as f:
-            content = json.dumps(self.v2ray_config, indent=4)
+            content = json.dumps(self.xray_config, indent=4)
             f.write(content)
-
-
-class NginxConfig:
-    def __init__(self, nginx_config: str) -> None:
-        self.nginx_config = nginx_config
-        
-    def update_nginx_config(self, domain_name: str, v2ray_port: int, ws_path: str):
-        domain_cert_path = "/etc/letsencrypt/live/{}/fullchain.pem".format(domain_name)
-        domain_key_path = "/etc/letsencrypt/live/{}/privkey.pem".format(domain_name)
-        
-        loaded_cert_path = re.findall(r"/etc/letsencrypt/live/.+/fullchain\.pem", self.nginx_config)[0]
-        loaded_key_path = re.findall(r"/etc/letsencrypt/live/.+/privkey\.pem", self.nginx_config)[0]
-    
-        loaded_ws_path = re.findall(r"location .+ {", self.nginx_config)[0]
-        loaded_v2ray_port = re.findall(r"http://127.0.0.1:\d+", self.nginx_config)[0]
-        
-        loaded_server_name = re.findall(r"server_name           .+;", self.nginx_config)[0]
-        
-        self.nginx_config = self.nginx_config.replace(loaded_cert_path, domain_cert_path)
-        self.nginx_config = self.nginx_config.replace(loaded_key_path, domain_key_path)
-        self.nginx_config = self.nginx_config.replace(loaded_ws_path, f"location {ws_path} {{")
-        self.nginx_config = self.nginx_config.replace(loaded_v2ray_port, f"http://127.0.0.1:{v2ray_port}")
-        self.nginx_config = self.nginx_config.replace(loaded_server_name, f"server_name           {domain_name};")
-        
-    def save_nginx_config(self, save_path: str):
-        with open(save_path, "w") as f:
-            f.write(self.nginx_config)
             
     
 class DNSSolver:
@@ -188,25 +164,25 @@ def create_dns_record(password: str, dns_name: str):
     
     dns_solver = DNSSolver(zone_id=zone_id, token=cf_token)
     
-    trojan_dns = "trojan." + "v4." + dns_name
-    v2ray_dns = "v2ray." + "v4." + dns_name
+    xray_dns = "genshin-" + "v4-" + dns_name
+    cdn_dns = "cdn-genshin-" + "v4-" + dns_name
     
     ipv4 = get_ipv4()
     
-    if dns_solver.check_dns_exist(trojan_dns):
-        print_red("Trojan DNS already exists, check {}".format(trojan_dns))
+    if dns_solver.check_dns_exist(xray_dns):
+        print_red("XRay DNS already exists, check {}".format(xray_dns))
         exit(1)
         
-    if dns_solver.check_dns_exist(v2ray_dns):
-        print_red("V2ray DNS already exists, check {}".format(v2ray_dns))
+    if dns_solver.check_dns_exist(cdn_dns):
+        print_red("CDN-XRay DNS already exists, check {}".format(cdn_dns))
         exit(1)
         
-    dns_solver.create_dns_record(trojan_dns, ipv4)
-    dns_solver.create_dns_record(v2ray_dns, ipv4)
+    dns_solver.create_dns_record(xray_dns, ipv4)
+    dns_solver.create_dns_record(cdn_dns, ipv4, proxied=True)
     
         
     print_green("Create DNS record successfully!")
-    return trojan_dns, v2ray_dns
+    return xray_dns, cdn_dns
     
 
 if __name__ == "__main__":
@@ -219,23 +195,13 @@ if __name__ == "__main__":
     
     password = input("Please input the password:")
 
-    trojan_name, v2ray_name = create_dns_record(password, dns_name)
-    v2_config, trojan_config, nginx_config = get_configs(password)
-    
-    v2_config = json.loads(v2_config)
-    trojan_config = json.loads(trojan_config)
-    
-    v2ray_config_manager = V2rayConfig(v2_config)
-    trojan_config_manager = TrojanConfig(trojan_config)
-    nginx_config_manager = NginxConfig(nginx_config)
-    
-    v2ray_config_manager.update_v2ray_config()
-    trojan_config_manager.update_trojan_config(trojan_name)
-    nginx_config_manager.update_nginx_config(v2ray_name, v2ray_config_manager.inner_port, v2ray_config_manager.ws_path)
-    
-    v2ray_config_manager.save_v2ray_config("./v2ray_config.json")
-    trojan_config_manager.save_trojan_config("./trojan_config.json")
-    nginx_config_manager.save_nginx_config("./nginx_config.conf")
+    xray_name, cdn_name = create_dns_record(password, dns_name)
+
+    xray_config, user_dict = get_configs(password)
+    xray_config = json.loads(xray_config)
+    xray_config_manager = XrayConfig(xray_config)
+    xray_config_manager.update_xray_config(xray_name, cdn_name, user_dict)
+    xray_config_manager.save_xray_config("./vless_config.json")
     
     print_green("Update configs successfully!")
     
